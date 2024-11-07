6ecb21735b8515ca9f5a27248d7abac20aea88c6
Ignacio Pita <33305389+KillerX629@users.noreply.github.com>
2024-11-06 21:00:36 -0300

    añadido tailwind en layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    import '../app.postcss';
+    let children = $props()
+</script>
+
+
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/core";
-  import '../app.postcss';
+
 
   let name = $state("");
   let greetMsg = $state("");
